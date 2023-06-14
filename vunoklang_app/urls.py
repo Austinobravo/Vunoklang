aeb910020624fcbd6caf8a2ad6ae046172673cba
BeQool98 <davidistifanus82@gmail.com>
2023-06-14 05:42:32 -0700

    Home and about page

--- vunoklang_app/urls.py
+++ vunoklang_app/urls.py
@@ -6,5 +6,6 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('event_details/', event_details, name="event_details"),
-    path('event_listing/', event_listing, name="event_listing")
+    path('event_listing/', event_listing, name="event_listing"),
+    path('about_page/', about_page, name="about_page"),
 ]
--- vunoklang_app/views.py
+++ vunoklang_app/views.py
@@ -10,6 +10,12 @@ def event_details(request):
     context = {}
     return render(request, "event-detail.html", context)
 
+
 def event_listing(request):
     context = {}
     return render(request, "event-listing.html", context)
+
+
+def about_page(request):
+    context = {}
+    return render(request, "about_page.html", context)
